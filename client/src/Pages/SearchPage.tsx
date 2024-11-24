d8e9a389881dc7ac9ebf7d9cfb39d50ebd8f64a9
samson <samsonmendonca96@gmail.com>
2024-11-24 10:31:01 +0530

    changing search input

--- client/src/Pages/SearchPage.tsx
+++ client/src/Pages/SearchPage.tsx
@@ -6,7 +6,7 @@ import { Link, useParams } from "react-router-dom";
 export default function SearchPage() {
   var {search} =useParams();
   var search2 = search.charAt(0).toUpperCase() + search?.slice(1);
-  console.log(search2);
+
   const [places, setplaces] = useState([]);
   useEffect(async () => {
     if(!search){
